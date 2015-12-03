dbe4ae623fc07f5501fcffd5d160b875ae24d9de
Jope <joni.porkka@yahoo.com>
2015-12-03 07:33:05 +0200

    Exercise updates

---
+++ tutorial/6.2.2/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+namespace Animals
+{
+    enum Animals
+    {
+        CHICKEN,
+        DOG,
+        CAT,
+        ELEPHANT,
+        DUCK,
+        SNAKE,
+        MAX_ANIMALS
+    };
+}
+
+int main ()
+{
+    int legs[Animals::MAX_ANIMALS] { 2, 4, 4, 4, 2, 0 };
+    std::cout << "An elephant has " << legs[Animals::ELEPHANT] << " legs" << std::endl;
+
+    return 0;
+}
